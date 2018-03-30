757131bfc552cdea788e550f69a8f7c0172b51c8
i32qutoj <i32qutoj@uco.es>
2018-03-30 10:09:10 +0200

    Final commit

--- holamundo.c
+++ holamundo.c
@@ -1,5 +1,6 @@
-#include "./includes/miinclude.h"
+#include <string.h>
 int main(){
-	printf("Hola mundo\n");
+	char c[100]= "Hola mundo";
+	printf("%s\n", strcat(c,", soy i32qutoj"));
 	return 0;
 }
